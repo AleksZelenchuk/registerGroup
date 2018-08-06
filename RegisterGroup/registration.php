6121a6058efac97e4112608181c4a49111cd0270
AleksZelenchuk <zelenchuk.oleks@gmail.com>
2018-08-06 13:31:32 +0300

    Update registration.php

--- RegisterGroup/registration.php
+++ RegisterGroup/registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Fantronix_RegisterGroup',
+    'Namespace_RegisterGroup',
     __DIR__
 );
